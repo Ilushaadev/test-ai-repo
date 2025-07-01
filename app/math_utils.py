1a1ce20dc018f7bb8d2021f5ff506fce7e9b3a0d
Ilya Kanterman <ilya.kanterman@gmail.com>
2025-07-01 22:11:40 +0300

    new testdsa

--- app/math_utils.py
+++ app/math_utils.py
@@ -147,10 +147,10 @@ def analyze_word(wordy):
         return "Test3"
 
 
-def test2(ilya = "test2"):
+def test2(ilya = "test22"):
     new = ilya
     return print(f'{new}')
 
-def test3(ilya = "test2"):
+def test3(ilya = "test23"):
     new = ilya
     return print(f'{new}')
