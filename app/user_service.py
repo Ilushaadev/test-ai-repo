0c3c764e463ea007201e30def7319ac6e6e7e330
Ilya Kanterman <ilya.kanterman@gmail.com>
2025-07-07 17:57:07 +0300

    new_user_finaly10

--- app/user_service.py
+++ app/user_service.py
@@ -19,7 +19,7 @@ def get_user_profile_summary(user):
 
     return f"{name} ({email}) - {role} [{active}]"
 
-def new_user_created6():
+def new_user_created1():
     new_user = 1
     if new_user != 0:
         return print("new user created")
