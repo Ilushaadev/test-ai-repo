989f3fd9ba248a19c5f44345a61352bca6165f38
Ilya Kanterman <ilya.kanterman@gmail.com>
2025-07-07 14:49:45 +0300

    new_user_final

--- app/math_utils.py
+++ app/math_utils.py
@@ -42,5 +42,5 @@ def calculate_somthing():
 def calculate_somthing4():
     return print("test4")
 
-def calculate_somthing3():
+def calculate_somthing2():
     return print("test4")
--- app/user_service.py
+++ app/user_service.py
@@ -19,7 +19,7 @@ def get_user_profile_summary(user):
 
     return f"{name} ({email}) - {role} [{active}]"
 
-def new_user_create():
+def new_user_creates():
     new_user = 0
     if new_user != 0:
         return print("new user created")
